a3a3aa8670484a7d01df0b682ec61de65548dd52
yuan <594726016@qq.com>
2017-12-22 18:22:48 +0800

    更新动画demo

--- demos/demo_animated.js
+++ demos/demo_animated.js
@@ -1,41 +1,46 @@
 import React, { Component } from 'react';
-import { Animated,Text } from 'react-native';
+import { Animated, Text, Button, View, TouchableHighlight } from 'react-native';
 
-export  class FadeInView extends Component {
+export class FadeInView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      fadeAnim: new Animated.Value(0),          // 透明度初始值设为0
+      fadeAnim: new Animated.Value(0),        // 透明度初始值设为0
     };
   }
+
   componentDidMount() {
-    Animated.timing(                            // 随时间变化而执行的动画类型
+    Animated.spring(                            // 随时间变化而执行的动画类型
       this.state.fadeAnim,                      // 动画中的变量值
       {
-        toValue: 1,                             // 透明度最终变为1，即完全不透明
+        toValue: 300,                   // 透明度最终变为1，即完全不透明
+        duration: 1000,
       }
-    ).start();                                  // 开始执行动画
+    ).start();                               // 开始执行动画
   }
+  
   render() {
     return (
       <Animated.View                            // 可动画化的视图组件
         style={{
           ...this.props.style,
-          opacity: this.state.fadeAnim,          // 将透明度指定为动画变量值
+          width: this.state.fadeAnim,          // 将透明度指定为动画变量值
+          height: this.state.fadeAnim,
         }}
       >
-        {this.props.children}
+      {this.props.children}
       </Animated.View>
     );
   }
 }
 
-export default class DemoAnimated extends Component{
+export default class DemoAnimated extends Component {
   render() {
     return (
-      <FadeInView style={{width: 250, height: 50, backgroundColor: 'powderblue'}}>
-        <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}>Fading in</Text>
-      </FadeInView>
+      <View>
+        <FadeInView style={{ width: 100, height: 100, backgroundColor: '#ff5d5e' }}>
+        </FadeInView>
+      </View>
     )
   }
 }
